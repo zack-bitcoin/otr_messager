dd7c796280fea1cb3534705f2e28b89b46351397
zack-bitcoin <zack.bitcoin@gmail.com>
2020-04-29 10:34:51 -0400

    got the api working.

--- apps/blog/src/api_handler.erl
+++ apps/blog/src/api_handler.erl
@@ -1,12 +1,13 @@
 -module(api_handler).
 -export([init/3, handle/2, terminate/3, doit/1,
-         init/2]).
+         init/2,
+         test/0]).
 
 init(A, B) ->
     handle(A, B).
 handle(Req, State) ->
     {ok, Data, Req2} = cowboy_req:read_body(Req),
-    {IP, _} = cowboy_req:peer(Req2),
+    %{IP, _} = cowboy_req:peer(Req2),
     true = is_binary(Data),
     A = packer:unpack(Data),
     B = doit(A),
@@ -20,10 +21,18 @@ terminate(_Reason, _Req, _State) -> ok.
 doit({test}) ->    
     {ok, 22};
 doit({send, Msg, To}) ->
+    65 = size(To),
+    true = is_binary(To),
+    true = is_binary(Msg),
     inbox:send(Msg, To);
-doit({read, Nonce, To, Delay}) ->
+doit({read, Nonce, To, Delay0}) ->
+    true = is_integer(Nonce),
+    true = is_integer(Delay0),
+    true = Delay0 > -1,
+    65 = size(To),
+    true = is_binary(To),
 %. gets all the messages for a particular user with a nonce higher than Nonce. Delay is how long to maintain this http request relationship, so if a message appears during this time period it can get immediately sent.
-    Delay = min(Delay, 60),%maximum of a 1 minute relationship.
+    Delay = min(Delay0, 60),%maximum of a 1 minute relationship.
     Return = read_thread(Nonce, To, Delay, now2()),
     {ok, Return};
 
@@ -36,7 +45,8 @@ doit(X) ->
 now2() ->
     erlang:timestamp().
 read_thread(Nonce, To, Delay, Then) ->
-    Diff = timer:now_diff(Then, now2()),
+    Diff = timer:now_diff(now2(), Then),
+    %io:fwrite(Diff),
     R = inbox:read(To, Nonce),
     L = length(R),
     if
@@ -47,3 +57,15 @@ read_thread(Nonce, To, Delay, Then) ->
             read_thread(Nonce, To, Delay, Then)
     end.
             
+params() -> crypto:ec_curve(secp256k1).
+generate_keys() -> 
+    crypto:generate_key(ecdh, params()).
+
+test() ->
+    {To, _} = generate_keys(),
+    
+    spawn(fun() ->
+                  timer:sleep(500),
+                  doit({send, <<"hi">>, To})
+          end),
+    doit({read, 0, To, 6}).
--- todo.md
+++ todo.md
@@ -1,1 +1,4 @@
 having messages appear immediately.
+
+
+we need some thread to trigger auto-garbage collection for the inbox gen_server.
